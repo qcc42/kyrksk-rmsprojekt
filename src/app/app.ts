135d5d67f7cd911a3269909900850f1527a808f3
qcc42 <qcc.1992@gmail.com>
2025-12-20 16:34:16 +0200

    Added image.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
-
 @Component({
   selector: 'app-root',
   imports: [],
   template: `
     <h1>Default</h1>
+    <div><img  class="images" src="Untitled.jpeg"></div>
   `,
   styleUrls: ['./app.css'],
 })
