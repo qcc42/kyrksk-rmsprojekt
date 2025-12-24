632cd56f3f3e9130925853145f2190b1802ce864
qcc42 <qcc.1992@gmail.com>
2025-12-24 10:59:43 +0200

    Added angular materials, simple example Toggle.

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { MatSliderModule } from '@angular/material/slider';
+import {Component} from '@angular/core';
+@Component ({
+  imports: [
+    MatSliderModule,
+  ]
+})
+class AppModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {Image} from 'src/app/image/image';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-root',
-  imports: [Image],
+  imports: [Image, MatSlideToggleModule],
   template: `
     <h1>Default</h1>
     <app-image></app-image>
+<mat-slide-toggle>Toggle me!</mat-slide-toggle>
   `,
   styleUrls: ['./app.css'],
 })
